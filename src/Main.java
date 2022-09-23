f4203b0d79afab4d599f160702c900b3dfe53320
SikderSakhawat <114007137+SikderSakhawat@users.noreply.github.com>
2022-09-22 23:47:10 -0400

    Added my favorite food into my code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-public class Main {
-    public static void main(String[] args){
+public class Main
+{
+    public static void Main(String[] args)
+    {
         System.out.println("Sikder Sakhawat");
         System.out.println("Everything Everywhere At Once");
+        System.out.println("Pizza");
     }
 }
